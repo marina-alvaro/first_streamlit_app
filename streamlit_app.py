9ff80917117aad9074a44f543ad79b624596aafa
marina-alvaro <66832708+marina-alvaro@users.noreply.github.com>
2023-11-11 00:48:12 +0100

    Add second button

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,7 @@ except URLError as e:
 # streamlit.text(fruityvice_response.json()) # just writes the data to the screen
 
 # Don't run anything past here while we troubleshoot
-streamlit.stop()
+#streamlit.stop()
 
 # import snowflake.connector
 # Trial account Metadata
@@ -76,9 +76,16 @@ if streamlit.button('Get Fruit Load list'):
     streamlit.dataframe(my_data_rows)
 
 # Allow the end user to add a fruit to the list
-add_my_fruit = streamlit.text_input('What fruit would you like to add?')
-streamlit.write('Thanks for adding ', add_my_fruit)
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("insert into fruit_load_list values ('from streamlit')");
+        return "Thanks for adding " + new_fruit
 
-# This will not work correctly but just go with it for now
-my_cur.execute("insert into fruit_load_list values ('from streamlit')");
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a Fruit to the List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    back_from_function = insert_row_snowflake(add_my_fruit)
+    streamlit.text(back_from_function)
+
+
 
